7defa3b4bef0371787c5ddf0c3759237b383735a
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-11-09 20:32:24 +0800

    Create home screen

--- screens/completion/HomeScreen.js
+++ screens/completion/HomeScreen.js
@@ -1,6 +1,10 @@
 import React, {  Component } from 'react'
-import { StyleSheet, Text, View, ScrollView, FlatList, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Linking, Alert, Platform } from 'react-native'
 import firebase from 'firebase'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+import { PHONE_NUMBER } from '../../env'
 
 
 class HomeScreen extends Component {
@@ -8,9 +12,10 @@ class HomeScreen extends Component {
         super(props)
         this.state = {
             cards: [],
-            loaded: false
+            loaded: false,
         }
         this.getCards = this.getCards.bind(this)
+        this.handleContactUs = this.handleContactUs.bind(this)
     }
     async getCards(cb) {
         const snapshot = await firebase.firestore().collection('cards').get()
@@ -23,28 +28,101 @@ class HomeScreen extends Component {
                 cards: JSON.stringify(data),
                 loaded: true
             });
-            console.log(JSON.stringify(data))
         })
     }
-    render() {
-        const Item = ({ title, text }) => (
-            <View style={styles.item}>
-                <Text style={styles.titleText}>{title}</Text>
-            </View>
-        )
-        const renderItem = ({ item, index }) => {
-            console.log(JSON.stringify(index))
-            return(
-                <Item title={item.title} />
+    handleClick(url){
+        Linking.openURL(url)
+    }
+    handleContactUs(){
+        const handleCall = (ios) => {
+            if (ios){
+                Linking.openURL('telprompt:' + PHONE_NUMBER)
+            }else{
+                Linking.openURL('tel:' + PHONE_NUMBER)
+            }
+        }
+        const handleEmail = () => {
+            Linking.openURL('mailto:info@pwa-chk.org.pk')
+        }
+        const handleWeb = () => {
+            Linking.openURL('https://pwa-chk.org.pk/')
+        }
+        if (Platform.OS === "android"){
+            Alert.alert(
+                "Contact us",
+                "How would you like to contact us?",
+                [
+                    {
+                        text: "Call us",
+                        onPress: () => {handleCall(false)}
+                    },
+                    {
+                        text: "Email us",
+                        onPress: () => {handleEmail()}
+
+                    },
+                    {
+                        text: "Visit our website",
+                        onPress: () => {handleWeb()}
+                    },
+                ],
+                {onDismiss: () => {}}
+            )
+        }else {
+            Alert.alert(
+                "Contact us",
+                "How would you like to contact us?",
+                [
+                    {
+                        text: "Call us",
+                        onPress: () => {handleCall(true)}
+                    },
+                    {
+                        text: "Email us",
+                        onPress: () => {handleEmail()}
+                    },
+                    {
+                        text: "Visit our website",
+                        onPress: () => {handleWeb()}
+                    },
+                    {
+                        text: "Cancel",
+                        style: 'cancel'
+                    }
+                ]
             )
         }
+    }
+    render() {
+        const Item = ({ title, text, url }) => (
+            <View style={styles.item}>
+                <View onStartShouldSetResponder={() => {this.handleClick( url)}}>
+                    <Text style={styles.titleText}>{title}</Text>
+                    <Text style={styles.itemText}>{text}</Text>
+                </View>
+            </View>
+        )
+        const renderItem = ({ item }) => (
+            <Item url={item.url} title={item.title} text={item.text} />
+        )
         const HeaderItem = () => {
             return(
-                <View>
-                    <Text style={styles.titleText}>Hello world, {`\n ${firebase.auth().currentUser.email}`}</Text>
+                <View style={{display: 'flex', flexDirection: 'row'}}>
+                    <View style={{maxWidth: '95%'}}>
+                        <Text style={styles.titleText}>Hello,{`\n`}<Text style={styles.itemText}>Here's the latest news from PWA</Text></Text> 
+                    </View>
+                    <Ionicons onPress={this.handleContactUs} name="ios-call" size={32} color="tomato" />
                 </View>
             )
         }
+        const Seperator = () => (
+            <View
+                style={{
+                    backgroundColor: '#c3c3c3',
+                    height: 2,
+                }}
+            />
+        )
         if (this.state.loaded){
             return(
                 <FlatList
@@ -53,6 +131,7 @@ class HomeScreen extends Component {
                     data={JSON.parse(this.state.cards)}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id}
+                    ItemSeparatorComponent={Seperator}
                 />
             )
         }else{
@@ -74,16 +153,16 @@ const styles = StyleSheet.create({
         paddingTop: '15%',
     },
     titleText: {
-        fontWeight: 'bold',
-        fontSize: 30
+        fontWeight: '600',
+        fontSize: 30,
+        padding: 8,
     },
     item: {
-        backgroundColor: '#f3f3f3',
-        padding: 20,
+        padding: '5%',
         marginVertical: 8,
-        marginHorizontal: 16,
     },
-    loadingText: {
-
+    itemText: {
+        fontSize: 22,
+        fontWeight: '400'
     }
 })

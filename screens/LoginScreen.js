957943c6c1a82d11dd9b5f6a83f0807509bf8746
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-11-09 20:31:09 +0800

    Bug fixes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,20 +7,22 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TextInput } from 'react-native-gesture-handler';
 import Btn from "../components/Btn";
+import {REACT_APP_API_KEY, REACT_APP_APP_ID, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_MEAUREMENT_ID, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET} from '../env' 
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1lpIDveOquJa5I4dikobpWPK-iwqf_4w",
-    authDomain: "blooddonation-c247b.firebaseapp.com",
-    databaseURL: "https://blooddonation-c247b.firebaseio.com",
-    projectId: "blooddonation-c247b",
-    storageBucket: "blooddonation-c247b.appspot.com",
-    messagingSenderId: "978911600721",
-    appId: "1:978911600721:web:b364a8fcc2df8722241870",
-    measurementId: "G-Z3KDT7XSGT"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEAUREMENT_ID
 };
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
 
 const LogIn = (email, password, callback) => {
     firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
